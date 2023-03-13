77216f4a2f24b47fe36ea78a493ba809927bf67a
sowmik-sec <sowmik.sec@gmail.com>
2023-03-13 19:21:06 +0600

    server and mongodb initialized

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xgh8h2c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.get("/", (req, res) => {
+  res.send("ema john server is running");
+});
+
+app.listen(port, () => {
+  console.log(`ema john running on: ${port}`);
+});
