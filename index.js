c532ceead9e03e0d8df46612d64133e2ad8b404e
sowmik-sec <sowmik.sec@gmail.com>
2023-03-13 19:42:43 +0600

    add data from json to database and load on the UI

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    const productCollection = client.db("emaJohn").collection("products");
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
+
 app.get("/", (req, res) => {
   res.send("ema john server is running");
 });
