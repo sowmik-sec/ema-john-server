a3da62cd44144d18324e01c949851eaacc0b369a
sowmik-sec <sowmik.sec@gmail.com>
2023-03-14 10:14:48 +0600

    making api for pagination

--- index.js
+++ index.js
@@ -20,12 +20,15 @@ async function run() {
   try {
     const productCollection = client.db("emaJohn").collection("products");
     app.get("/products", async (req, res) => {
-      const page = req.query.page;
-      const size = req.query.size;
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
       console.log(page, size);
       const query = {};
       const cursor = productCollection.find(query);
-      const products = await cursor.toArray();
+      const products = await cursor
+        .skip(page * size)
+        .limit(size)
+        .toArray();
       const count = await productCollection.estimatedDocumentCount();
       res.send({ count, products });
     });
